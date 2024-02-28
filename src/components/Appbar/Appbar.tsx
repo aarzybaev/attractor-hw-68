fb9a7601df9140e954e5eb824808a2c1d20f6334
Askhad Arzybaev <aarzybaev@gmail.com>
2024-02-28 13:44:44 +0600

    created Appbar component

---
+++ src/components/Appbar/Appbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+const Appbar: React.FC = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <div className="container-fluid">
+        <span className="navbar-brand">TODO-APP</span>
+        <div className="collapse navbar-collapse">
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Appbar;
